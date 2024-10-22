cbdea1cf7ff5c0b3af0e6a96c142919f893911cb
noracami <kerke.2011@gmail.com>
2024-10-23 03:36:27 +0800

    🔨 run on PORT

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import os
 from aoe_find_match_result_flask_backend import app
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host="0.0.0.0")
